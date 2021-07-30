6c8cb94926d6a57b970bf23b50724b0f6b5ed382
Bruce Graham <bruce.graham@zoetis.com>
2021-07-30 14:49:04 -0400

    cmd spaces

--- QR_codes/qr_decoder/qr_scanner.py
+++ QR_codes/qr_decoder/qr_scanner.py
@@ -59,11 +59,12 @@ def read_barcodes(frame):
 
 if __name__ == '__main__':
 
+    camera = cv2.VideoCapture(0)
     # turn off the LED
     if (os.name == 'nt'):
         print("NT")
     elif (os.name == 'posix'):
-        lstCmd = ["./OneDrive_1_7-30-2021/SP_V4L2_API-2021-07-19/Demo_V4L2/bin/SPCA_v4l2_tool_GNU_x64", "-D 0", "-w", "-a 2043", "-e 0"]
+        lstCmd = ["./OneDrive_1_7-30-2021/SP_V4L2_API-2021-07-19/Demo_V4L2/bin/SPCA_v4l2_tool_GNU_x64", "-D0", "-w", "-a2043", "-e0"]
         print("call ")
         for x in range(len(lstCmd)):
             print(lstCmd[x])
@@ -72,7 +73,6 @@ if __name__ == '__main__':
         time.sleep(5)
         print("... end of sleep")
 
-    camera = cv2.VideoCapture(0)
     ret, frame = camera.read()
 
     while ret:
