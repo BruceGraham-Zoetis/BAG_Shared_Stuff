1caf514728ba6800a4e180266443d83d945e7423
Bruce Graham <bruce.graham@zoetis.com>
2021-07-30 14:33:09 -0400

    cmd stuff

--- QR_codes/qr_decoder/qr_scanner.py
+++ QR_codes/qr_decoder/qr_scanner.py
@@ -8,6 +8,7 @@ import cv2
 from pyzbar import pyzbar
 import json
 import os
+import time
 
 def read_barcodes(frame):
     try:
@@ -60,9 +61,12 @@ if __name__ == '__main__':
 
     # turn off the LED
     if (os.name == 'nt'):
-        pass
-    else:
+        print("NT")
+    elif (os.name == 'posix'):
         subprocess.call(["./OneDrive_1_7-30-2021/SP_V4L2_API-2021-07-19/Demo_V4L2/bin/SPCA_v4l2_tool_GNU_x64", "-D 0", "-w", "-a 2043", "-e 0"])
+        print("sleeping 10 seconds...")
+        time.sleep(50)
+        print("... end of sleep")
 
     camera = cv2.VideoCapture(0)
     ret, frame = camera.read()
