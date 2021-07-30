7ecbde26a0b3d09d56d6b177af35623ccc94a2c6
Bruce Graham <bruce.graham@zoetis.com>
2021-07-30 14:41:53 -0400

    more call

--- QR_codes/qr_decoder/qr_scanner.py
+++ QR_codes/qr_decoder/qr_scanner.py
@@ -64,7 +64,9 @@ if __name__ == '__main__':
         print("NT")
     elif (os.name == 'posix'):
         lstCmd = ["./OneDrive_1_7-30-2021/SP_V4L2_API-2021-07-19/Demo_V4L2/bin/SPCA_v4l2_tool_GNU_x64", "-D 0", "-w", "-a 2043", "-e 0"]
-        print("call " + lstCmd)
+        print("call ")
+        for x in range(len(lstCmd)):
+            print(lstCmd[x])
         subprocess.call(lstCmd)
         print("sleeping 5 seconds...")
         time.sleep(5)
