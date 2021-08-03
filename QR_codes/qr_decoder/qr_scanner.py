3ea694d9a4bdcfe024b1023b3dce89dffba84dc9
BruceGraham-Zoetis <bruce.graham@zoetis.com>
2021-08-03 15:19:04 -0400

    When QR code is detected: + Added beep + Displays and prints the QR code contents + Closes the window Turning off auto focus.

--- QR_codes/qr_decoder/qr_scanner.py
+++ QR_codes/qr_decoder/qr_scanner.py
@@ -2,37 +2,48 @@
 # 
 # # see https://towardsdatascience.com/building-a-barcode-qr-code-reader-using-python-360e22dfb6e5
 
-# pip3 install pyzbar
-# pip3 install playsound
-# pip install v4l2ctl
-#   v4l2ctl is a python package to control v4l2 drivers.
-#   https://pypi.org/project/v4l2ctl/
-#   https://v4l2ctl.readthedocs.io/en/latest/
-#   
+# Computer Vision
+# ==========================
+#   $ pip3 install pyzbar
+#
+# Video for Linux
+# ==================
+# v4l2ctl is a python package to control v4l2 drivers.
+# https://pypi.org/project/v4l2ctl/
+# https://v4l2ctl.readthedocs.io/en/latest/
+#
+# console app
+# --------------------
+# sudo apt install v4l-utils
+#    $ v4l2-ctl --device=/dev/video0 -l
+# Python lib
+# -------------------
+#    $ sudo pip3 install v4l2ctl
 
 #import libraries
-import subprocess
+#import subprocess
 import cv2
 from pyzbar import pyzbar
 import json
 import os
-import playsound
 import time
+import v4l2ctl
+
+import sys
+sys.path.append('../../audio_control')
+#sys.path.insert(1, '/.../../audio_control')
+import audio_play
 
 def decode_display_barcodes(frame):
+    bFound = False
+    barcode_info = ""
+
     try:
         barcodes = pyzbar.decode(frame)
     except:
-        return
+        return bFound, frame, barcode_info
 
     for barcode in barcodes:
-        try:
-            # Detected some data in the QR code, so beep.
-            playsound.playsound('beep-08b.wav')
-            time.sleep(0.2)
-        except:
-            pass
-
         x, y , w, h = barcode.rect
 
         barcode_info = barcode.data.decode('utf-8')
@@ -66,12 +77,14 @@ def decode_display_barcodes(frame):
             except:
                 txtDisplay = "data: ?"
             cv2.putText(frame, txtDisplay, (x + 6, y + h + 120), font, 1.0, (255, 255, 255), 1)
+            bFound = True
 
         except:
             txtDisplay = barcode_info
             cv2.putText(frame, txtDisplay, (x + 6, y + h + 30), font, 1.0, (255, 255, 255), 1)
+            bFound = True
 
-    return frame
+    return bFound, frame, barcode_info
 
 if __name__ == '__main__':
     camera = cv2.VideoCapture(0)
@@ -80,18 +93,40 @@ if __name__ == '__main__':
     if (os.name == 'nt'):
         print("NT")
     elif (os.name == 'posix'):
+        """
         lstCmd = ["./OneDrive_1_7-30-2021/SP_V4L2_API-2021-07-19/Demo_V4L2/bin/SPCA_v4l2_tool_GNU_x64", "-D0", "-w", "-a2043", "-e0"]
         print(*lstCmd)
         subprocess.call(lstCmd)
+        """
+        pass
+
+        # turn off the camera auto-focus
+    camera.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 
     ret, frame = camera.read()
+    iCount = 0
+    barcode_info = ""
 
     while ret:
-        frame = decode_display_barcodes(frame)
+        ret, frame, barcode_info = decode_display_barcodes(frame)
         cv2.imshow('Barcode/QR code reader', frame)
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-        ret, frame = camera.read()
+        if (ret):
+            iCount += 1
+            if (10 < iCount):
+                # after detecting the QR code for 10 times
+                try:
+                    # beep to let the user know the QR code was detected.
+                    audio_play.playWaveFileAndBlock('./beep-08b.wav')
+                    print(barcode_info)
+                    # show the window for 2 seconds
+                    time.sleep(2)
+                except:
+                    pass
+                break
+        else:
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+            ret, frame = camera.read()
 
     camera.release()
     cv2.destroyAllWindows()
