1a3bf51d939a9c58061dddf9962851569e61c01e
BruceGraham-Zoetis <bruce.graham@zoetis.com>
2021-08-25 10:51:48 -0400

    Moved template files to file compare is easier.

--- bag_vetscan/make_analyzer_dracula/test_run_analyzer_app.py
+++ bag_vetscan/make_analyzer_dracula/test_run_analyzer_app.py
@@ -12,9 +12,6 @@ strThisFilePath = os.path.dirname(__file__)
 sys.path.append(strThisFilePath)
 sys.path.append(strThisFilePath + "/analyzer_app")
 from analyzer_app.openapi_server import encoder
-from analyzer_app.openapi_server.controllers.CDBusDraculaService import CDBusDraculaService
-import socket
-import time
 
 
 def analyzer_apis():
@@ -37,31 +34,6 @@ def analyzer_apis():
     app.run(port=8080)
 
 
-
-def notify_hub_of_presence():
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-
-    # Enable port reusage so we will be able to run multiple clients and servers on single (host, port).
-    # Do not use socket.SO_REUSEADDR except you using linux(kernel<3.9): goto https://stackoverflow.com/questions/14388706/how-do-so-reuseaddr-and-so-reuseport-differ for more information.
-    # For linux hosts all sockets that want to share the same address and port combination must belong to processes that share the same effective user ID!
-    # So, on linux(kernel>=3.9) you have to run multiple servers and clients under one user to share the same (host, port).
-    # Thanks to @stevenreddie
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-
-    # Enable broadcasting mode
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-    # Set a timeout so the socket does not block
-    # indefinitely when trying to receive data.
-    server.settimeout(0.2)
-
-    message = b"dracula"
-    server.sendto(message, ("localhost", 37020))
-    print("message sent!", flush=True)
-
-
 if __name__ == '__main__':
     print("")
-
-    notify_hub_of_presence()    
     analyzer_apis()
--- bag_vetscan/make_analyzer_webclient/test_analyzer_webclient.py
+++ bag_vetscan/make_analyzer_webclient/test_analyzer_webclient.py
@@ -12,16 +12,41 @@ import time
 import datetime
 import signal
 import threading
+import socket
 
 
-async def analyzer_web_client():
+def notify_hub_of_presence():
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+    # Enable port reusage so we will be able to run multiple clients and servers on single (host, port).
+    # Do not use socket.SO_REUSEADDR except you using linux(kernel<3.9): goto https://stackoverflow.com/questions/14388706/how-do-so-reuseaddr-and-so-reuseport-differ for more information.
+    # For linux hosts all sockets that want to share the same address and port combination must belong to processes that share the same effective user ID!
+    # So, on linux(kernel>=3.9) you have to run multiple servers and clients under one user to share the same (host, port).
+    # Thanks to @stevenreddie
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+    # Enable broadcasting mode
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    # Set a timeout so the socket does not block
+    # indefinitely when trying to receive data.
+    server.settimeout(0.2)
+
+    message = b"dracula"
+    server.sendto(message, ("localhost", 37020))
+    print("sent name to Hub", flush=True)
+
+
+def thread_analyzer_web_client():
     print("Running web client")
 
     uriServer = "ws://localhost:8765"
 
     while True:
+        notify_hub_of_presence()
+
         try:
-            async with websockets.connect(uriServer) as websocket:
+            with websockets.connect(uriServer) as websocket:
                 # Close the connection when receiving SIGTERM.
                 this_loop = asyncio.get_event_loop()
                 this_loop.add_signal_handler(
@@ -34,7 +59,7 @@ async def analyzer_web_client():
                 strToServer = "set name dracula"
                 print("analyzer(client) -> server(Hub)\n\t" + strToServer)
                 try:
-                    await websocket.send(strToServer)
+                    websocket.send(strToServer)
                 except:
                     print("Disconnected send")
                     bConnected = False
@@ -43,13 +68,13 @@ async def analyzer_web_client():
                     strToServer = "get datetime"
                     print("analyzer(client) -> server(Hub)\n\t" + strToServer)
                     try:
-                        await websocket.send(strToServer)
+                        websocket.send(strToServer)
                     except:
                         print("Disconnected send")
                         bConnected = False
 
                     try:
-                        async for strFromClient in websocket:
+                        for strFromClient in websocket:
                             print("server(Hub) -> analyzer(client)\n\t" + strFromClient)
                             strMsg = "analyzer_client "
                             strMsg = datetime.datetime.now().time()
@@ -68,15 +93,12 @@ async def analyzer_web_client():
         time.sleep(5)
 
 
-def start_loop(loop, client):
-    loop.run_until_complete(client)
-    loop.run_forever()
 
 
 def start_thread_analyzer_web_client():
     new_loop = asyncio.new_event_loop()
 
-    thread = threading.Thread(target=start_loop, args=(new_loop, analyzer_web_client))
+    thread = threading.Thread(target = thread_analyzer_web_client)
     thread.start()
 
 
