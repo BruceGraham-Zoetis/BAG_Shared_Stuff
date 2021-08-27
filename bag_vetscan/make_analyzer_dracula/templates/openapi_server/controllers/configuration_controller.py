6dd5ac6c1a194426bc9dbb7dbc76dda765d3cef7
Bruce Graham <bruce.graham@zoetis.com>
2021-08-27 12:17:37 -0400

    Saved work files to templates

--- bag_vetscan/make_analyzer_dracula/templates/openapi_server/controllers/configuration_controller.py
+++ bag_vetscan/make_analyzer_dracula/templates/openapi_server/controllers/configuration_controller.py
@@ -1,51 +1,31 @@
 import connexion
 import six
 
-from openapi_server.models.inline_response400 import InlineResponse400  # noqa: E501
+from openapi_server.models.inline_response400 import InlineResponse400
 from openapi_server import util
 
-
-def configuration_factory_reset_put():  # noqa: E501
-    """configuration_factory_reset_put
-
-    Restore the analyzer to the state it was in when it left the factory. All settings and data are removed. # noqa: E501
-
-
-    :rtype: None
-    """
-    return 'do some magic!'
+import os, sys
+strThisFilePath = os.path.dirname(__file__)
+sys.path.append(strThisFilePath)
+from CDBusDraculaService import CDBusDraculaService
 
 
-def configuration_get():  # noqa: E501
-    """configuration_get
+def configuration_factory_reset_put():
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.configuration_factory_reset_put()
+    return strRtn
 
-    Request the configuration from the analyzer # noqa: E501
+def configuration_get():
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.configuration_get()
+    return strRtn
 
+def configuration_put(body):
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.configuration_put(body)
+    return strRtn
 
-    :rtype: object
-    """
-    return 'do some magic!'
-
-
-def configuration_put(body):  # noqa: E501
-    """configuration_put
-
-    Set the configuration of the analyzer # noqa: E501
-
-    :param body: 
-    :type body: 
-
-    :rtype: None
-    """
-    return 'do some magic!'
-
-
-def configuration_schema_get():  # noqa: E501
-    """configuration_schema_get
-
-    Request the configuration schema from the analyzer # noqa: E501
-
-
-    :rtype: object
-    """
-    return 'do some magic!'
+def configuration_schema_get():
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.configuration_schema_get()
+    return strRtn
--- bag_vetscan/make_analyzer_dracula/templates/openapi_server/controllers/measurement_channel_controller.py
+++ bag_vetscan/make_analyzer_dracula/templates/openapi_server/controllers/measurement_channel_controller.py
@@ -1,32 +1,56 @@
-    def channel_measurement_get_measurement_status():  # noqa: E501
-        return 'do some magic!'
+import connexion
+import six
+
+from openapi_server.models.inline_object import InlineObject
+from openapi_server.models.inline_object1 import InlineObject1
+from openapi_server.models.inline_response200 import InlineResponse200
+from openapi_server.models.inline_response2001 import InlineResponse2001
+from openapi_server.models.inline_response400 import InlineResponse400
+from openapi_server.models.measurement_result import MeasurementResult
+from openapi_server import util
+
+import os, sys
+strThisFilePath = os.path.dirname(__file__)
+sys.path.append(strThisFilePath)
+from CDBusDraculaService import CDBusDraculaService
 
 
-    def measurement_cancel_delete():  # noqa: E501
-        return 'do some magic!'
+def channel_measurement_get_measurement_status():
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.channel_measurement_get_measurement_status()
+    return strRtn
 
+def measurement_cancel_delete():
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.measurement_cancel_delete()
+    return strRtn
 
-    def measurement_consumable_consumable_uuid_post(consumable_uuid):  # noqa: E501
-        return 'do some magic!'
+def measurement_consumable_consumable_uuid_post(consumable_uuid):
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.measurement_consumable_consumable_uuid_post(consumable_uuid)
+    return strRtn
 
+def measurement_file_post(inline_object1):
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.measurement_file_post(inline_object1)
+    return strRtn
 
-    def measurement_file_post(inline_object1):  # noqa: E501
-        return 'do some magic!'
+def measurement_past_results_get(start_time, start_date, end_time, end_date):
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.measurement_past_results_get(start_time, start_date, end_time, end_date)
+    return strRtn
 
+def measurement_result_get():
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.measurement_result_get()
+    return strRtn
 
-    def measurement_past_results_get(start_time, start_date, end_time, end_date):  # noqa: E501
-        return 'do some magic!'
+def measurement_script_post(inline_object):
+    oDracula = CDBusDraculaService()
+    strRtn = oDracula.draculad.measurement_script_post(inline_object)
+    return strRtn
 
-
-    def measurement_result_get():  # noqa: E501
-        return 'do some magic!'
-
-
-    def measurement_script_post(inline_object):  # noqa: E501
-        return 'do some magic!'
-
-
-    def measurement_supported_consumables_get(self) -> str:
-        strRtn = self.draculad.measurement_supported_consumables_get()
-        return strRtn
-        
+def measurement_supported_consumables_get():
+    oDracula = CDBusDraculaService()
+    strConsumables = oDracula.draculad.measurement_supported_consumables_get()
+    return strConsumables
