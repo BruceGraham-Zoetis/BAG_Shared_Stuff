a59a68c0722a0187a0231723d365d0ad2f319fba
Bruce Graham <bruce.graham@zoetis.com>
2021-09-06 17:33:06 -0400

    Saved edited files to templates.

--- bag_vetscan/make_openapi_server/templates/openapi_server/controllers/CDBusDraculaService.py
+++ bag_vetscan/make_openapi_server/templates/openapi_server/controllers/CDBusDraculaService.py
@@ -20,11 +20,6 @@ import os, sys
 from pydbus import SessionBus
 import threading
 
-strThisFilePath = os.path.dirname(__file__)
-sys.path.append(strThisFilePath)
-sys.path.append(strThisFilePath + "/..")
-from models import inline_object1
-
 
 def get_analyzer_name() -> str:
     str_analyzer_name = "dracula"
@@ -36,7 +31,7 @@ def get_analyzer_dbus_request_name() -> str:
 
 
 """
-Class for the Dracula service. This is a singleton.
+Class for accessing the Dracula service. This is a singleton.
 
 """
 class CDBusDraculaService():
@@ -79,77 +74,6 @@ class CDBusDraculaService():
         self.draculad.measurement_id = str_value
 
 
-
-if __name__ == '__main__':
-    print("Test - Call all low-level 'Dracula' DBus service APIs")
-
-    oDracula = CDBusDraculaService()
-
-    # get and set measurement_id 
-    str_value = oDracula.measurement_id
-    print("measurement_id: %s" % oDracula.measurement_id)
-    oDracula.measurement_id = "xyz123"
-    print("measurement_id after set: %s" % oDracula.measurement_id)
-
-    print("\nfile: configuration_controller.py")
-    print("=========================================")
-    strRtn = oDracula.draculad.configuration_factory_reset_put()
-    print("configuration_factory_reset_put()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.configuration_get()
-    print("configuration_get()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.configuration_put("body")
-    print("configuration_put()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.configuration_schema_get()
-    print("configuration_schema_get()\n\t%s" % strRtn)
+g_dbus_dracula_service = CDBusDraculaService()
 
 
-    print("\nfile: control_channel_controller.py")
-    print("=========================================")
-    strRtn = oDracula.draculad.control_light_blink_put()
-    print("control_light_blink_put()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.control_light_off_put()
-    print("control_light_off_put()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.control_power_off_put()
-    print("control_power_off_put()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.control_power_reboot_put()
-    print("control_power_reboot_put()\n\t%s" % strRtn)
-
-
-    print("\nfile: measurement_channel_controller.py")
-    print("=========================================")
-    strRtn = oDracula.draculad.measurement_cancel_delete()
-    print("measurement_cancel_delete()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.measurement_consumable_consumable_uuid_post("consumable_uuid")
-    print("measurement_consumable_consumable_uuid_post()\n\t%s" % strRtn)
-
-    inline_object1 = inline_object1.InlineObject1(filename="dummy", local_vars_configuration=None)
-    strRtn = oDracula.draculad.measurement_file_post(inline_object1)
-    print("measurement_file_post()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.measurement_past_results_get("start_time", "start_date", "end_time", "end_date")
-    print("measurement_past_results_get()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.measurement_result_get()
-    print("measurement_result_get()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.measurement_script_post("inline_object")
-    print("measurement_script_post()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.measurement_supported_consumables_get()
-    print("measurement_supported_consumables_get()\n\t%s" % strRtn)
-
-
-    print("\nfile: status_channel_controller.py")
-    print("=========================================")
-    strRtn = oDracula.draculad.status_currently_activated_events_get()
-    print("status_currently_activated_events_get()\n\t%s" % strRtn)
-
-    strRtn = oDracula.draculad.status_operational_get()
-    print("status_operational_get()\n\t%s" % strRtn)
--- bag_vetscan/make_openapi_server/templates/openapi_server/controllers/measurement_channel_controller.py
+++ bag_vetscan/make_openapi_server/templates/openapi_server/controllers/measurement_channel_controller.py
@@ -1,49 +1,43 @@
-import connexion
-import six
+"""
+File: measurement_channel_controller.py
 
-from openapi_server.models.inline_object import InlineObject
-from openapi_server.models.inline_object1 import InlineObject1
-from openapi_server.models.inline_response200 import InlineResponse200
-from openapi_server.models.inline_response2001 import InlineResponse2001
-from openapi_server.models.inline_response400 import InlineResponse400
-from openapi_server.models.measurement_result import MeasurementResult
-from openapi_server import util
+Purpose: These are the openAPI functions.
+These functions call the analyzer "Dracula" DBus service.
+The "Dracula" DBus service will perform the low-level part of the openAPIs.
+"""
 
-import os, sys
-strThisFilePath = os.path.dirname(__file__)
-sys.path.append(strThisFilePath)
-from CDBusDraculaService import CDBusDraculaService
+
+import CDBusDraculaService
 
 
 def channel_measurement_get_measurement_status():
-    str_rtn = CDBusDraculaService.g_dbus_dracula_service.channel_measurement_get_measurement_status()
+    str_rtn = CDBusDraculaService.g_dbus_dracula_service.draculad.channel_measurement_get_measurement_status()
     return str_rtn
 
 def measurement_cancel_delete():
-    str_rtn = CDBusDraculaService.g_dbus_dracula_service.measurement_cancel_delete()
+    str_rtn = CDBusDraculaService.g_dbus_dracula_service.draculad.measurement_cancel_delete()
     return str_rtn
 
 def measurement_consumable_consumable_uuid_post(consumable_uuid):
-    str_rtn = CDBusDraculaService.g_dbus_dracula_service.measurement_consumable_consumable_uuid_post(consumable_uuid)
+    str_rtn = CDBusDraculaService.g_dbus_dracula_service.draculad.measurement_consumable_consumable_uuid_post(consumable_uuid)
     return str_rtn
 
 def measurement_file_post(inline_object1):
-    str_rtn = CDBusDraculaService.g_dbus_dracula_service.measurement_file_post(inline_object1)
+    str_rtn = CDBusDraculaService.g_dbus_dracula_service.draculad.measurement_file_post(inline_object1)
     return str_rtn
 
 def measurement_past_results_get(start_time, start_date, end_time, end_date):
-    str_rtn = CDBusDraculaService.g_dbus_dracula_service.measurement_past_results_get(start_time, start_date, end_time, end_date)
+    str_rtn = CDBusDraculaService.g_dbus_dracula_service.draculad.measurement_past_results_get(start_time, start_date, end_time, end_date)
     return str_rtn
 
 def measurement_result_get():
-    str_rtn = CDBusDraculaService.g_dbus_dracula_service.measurement_result_get()
+    str_rtn = CDBusDraculaService.g_dbus_dracula_service.draculad.measurement_result_get()
     return str_rtn
 
 def measurement_script_post(inline_object):
-    str_rtn = CDBusDraculaService.g_dbus_dracula_service.measurement_script_post(inline_object)
+    str_rtn = CDBusDraculaService.g_dbus_dracula_service.draculad.measurement_script_post(inline_object)
     return str_rtn
 
 def measurement_supported_consumables_get():
-    oDracula = CDBusDraculaService()
-    strConsumables = oDracula.draculad.measurement_supported_consumables_get()
-    return strConsumables
+    str_rtn = CDBusDraculaService.g_dbus_dracula_service.draculad.measurement_supported_consumables_get()
+    return str_rtn
